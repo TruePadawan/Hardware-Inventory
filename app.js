372abacfcdbe71791cec27ad0d85a5e2187d327f
Hermes <hermeschigoziri@gmail.com>
2023-05-22 18:22:03 +0100

    add routers to middleware chain

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+require("dotenv").config();
 
 const indexRouter = require("./routes/index");
+const hardwareTypeRouter = require("./routes/hardware_types");
+const hardwareRouter = require("./routes/hardware");
 
 const app = express();
 
@@ -19,6 +22,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
+app.use("/hardware", hardwareRouter);
+app.use("/hardware_types", hardwareTypeRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
