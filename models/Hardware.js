cbf3a641f39b9d30f7aaf850f687c08b56d46c8c
Hermes <hermeschigoziri@gmail.com>
2023-05-22 18:20:33 +0100

    models

---
+++ models/Hardware.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const HardwareSchema = new mongoose.Schema({
+	name: { required: true, type: String, maxLength: 100 },
+	desc: String,
+	img_filename: String,
+	price_usd: { type: Number, required: true },
+	hardware_type: {
+		required: true,
+		ref: "HardwareType",
+		type: mongoose.Schema.Types.ObjectId,
+	},
+	number_in_stock: { type: Number, required: true, default: 0 },
+});
+
+HardwareSchema.virtual("route_url").get(function () {
+	return `/hardware/${this._id}`;
+});
+
+HardwareSchema.virtual("image_url").get(function () {
+	return `/images/hardware/${this.img_filename}`;
+});
+
+module.exports = mongoose.model("Hardware", HardwareSchema);
---
+++ models/HardwareType.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const HardwareTypeSchema = new mongoose.Schema({
+	name: { required: true, type: String, maxLength: 40 },
+	desc: String,
+	img_filename: String,
+});
+
+HardwareTypeSchema.virtual("route_url").get(function () {
+	return `/hardware_types/${this._id}`;
+});
+
+HardwareTypeSchema.virtual("image_url").get(function () {
+	return `/images/hardware_types/${this.img_filename}`;
+});
+
+module.exports = mongoose.model("HardwareType", HardwareTypeSchema);
