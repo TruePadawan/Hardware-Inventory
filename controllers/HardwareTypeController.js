39e39f7d41b42c7209a057277d1e9018a8b0793b
Hermes <hermeschigoziri@gmail.com>
2023-05-23 15:23:44 +0100

    setup form for collecting info on hardware type to be created

--- controllers/HardwareTypeController.js
+++ controllers/HardwareTypeController.js
@@ -1,11 +1,15 @@
 const HardwareType = require("../models/HardwareType");
 
+// Display all hardware categories
 exports.get_hardware_types = function (req, res, next) {
 	HardwareType.find({}, { desc: -1 })
 		.exec()
 		.then((allHardwareTypes) => {
 			res.render("index", {
-				title: "Hardware Types",
+				title:
+					allHardwareTypes.length > 0
+						? "Hardware Types"
+						: "Create Hardware Type",
 				hardware_types: allHardwareTypes,
 				show_home_button: false,
 			});
@@ -13,4 +17,15 @@ exports.get_hardware_types = function (req, res, next) {
 		.catch(next);
 };
 
-// exports.create_hardware_type_get = function (req, res) {};
+// Display form for creating hardware type
+exports.create_hardware_type_get = function (req, res) {
+	res.render("hardware_type_form", {
+		title: "Create Hardware Type",
+		show_home_button: true,
+	});
+};
+
+// Handle POST request for creating hardware type
+exports.create_hardware_type_post = function (req, res) {
+	res.send("ROUTE NOT SET: POST /hardware_types/new");
+};
--- routes/hardware_types.js
+++ routes/hardware_types.js
@@ -5,14 +5,10 @@ const HardwareTypeController = require("../controllers/HardwareTypeController");
 router.get("/", HardwareTypeController.get_hardware_types);
 
 // GET request to create hardware type
-router.get("/new", function (req, res) {
-	res.send("ROUTE NOT SET: GET /hardware_types/new");
-});
+router.get("/new", HardwareTypeController.create_hardware_type_get);
 
 // POST request to create hardware type
-router.post("/new", function (req, res) {
-	res.send("ROUTE NOT SET: POST /hardware_types/new");
-});
+router.post("/new", HardwareTypeController.create_hardware_type_post);
 
 // GET request to show hardware type details
 router.get("/:hardwareTypeID", function (req, res) {
