c3f69977029e29642acdd9bcf2ad7f8c7709f498
Hermes <hermeschigoziri@gmail.com>
2023-05-26 14:23:14 +0100

    minor bugfix

--- routes/hardware.js
+++ routes/hardware.js
@@ -1,22 +1,21 @@
 const router = require("express").Router();
+const HardwareController = require("../controllers/HardwareController");
 
 router.get("/", function (req, res) {
 	res.redirect("/hardware_types");
 });
 
-// GET request to show hardware details
-router.get("/:hardwareID", function (req, res) {
-	res.send("ROUTE NOT SET: GET /hardware");
-});
-
 // GET request to add hardware under a hardware type
-router.get("/new", function (req, res) {
-	res.send("ROUTE NOT SET: GET /hardware/new");
-});
+router.get("/new", HardwareController.create_hardware_get);
 
 // POST request to add hardware under a hardware type
 router.post("/new", function (req, res) {
 	res.send("ROUTE NOT SET: POST /hardware/new");
+});
+
+// GET request to show hardware details
+router.get("/:hardwareID", function (req, res) {
+	res.send("ROUTE NOT SET: GET /hardware");
 });
 
 // GET request to delete hardware
