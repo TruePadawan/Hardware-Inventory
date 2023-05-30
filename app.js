edb205d34c6d286fb3a4d63b871ea0b94ae15f96
Hermes <hermeschigoziri@gmail.com>
2023-05-30 13:10:59 +0100

    use helmet to secure app

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const compression = require("compression");
+const helmet = require("helmet");
 require("dotenv").config();
 
 const indexRouter = require("./routes/index");
@@ -29,6 +30,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(compression());
+app.use(
+	helmet.contentSecurityPolicy({
+		directives: {
+			"script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+		},
+	})
+);
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
